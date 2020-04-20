4e0cf32efde17cbeb53f0cf3d5966172860851bc
Samuel Kozuch <samuel.kozuch@keboola.com>
2020-04-20 16:29:38 +0200

    STYLE: Changed tag to 0.1.3

--- src/dynamics/component.py
+++ src/dynamics/component.py
@@ -6,7 +6,7 @@ from kbc.env_handler import KBCEnvHandler
 from dynamics.client import DynamicsClient
 from dynamics.result import DynamicsWriter
 
-APP_VERSION = '0.1.3b1'
+APP_VERSION = '0.1.3'
 
 KEY_ORGANIZATIONURL = 'organization_url'
 KEY_ENDPOINT = 'endpoint'
