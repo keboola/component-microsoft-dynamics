c1bd7eefda72612d968ee3ac0afa19401f0b34fa
samuelhavelka <samuel.havelka@keboola.com>
2024-01-23 14:52:31 +0100

    ui refactor

--- src/dynamics/client.py
+++ src/dynamics/client.py
@@ -1,10 +1,10 @@
 import logging
-import os
 import requests
 import sys
 from kbc.client_base import HttpClientBase
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
+from urllib.parse import urljoin
 
 
 class DynamicsClient(HttpClientBase):
@@ -12,67 +12,57 @@ class DynamicsClient(HttpClientBase):
     MAX_RETRIES = 7
     PAGE_SIZE = 2000
 
-    def __init__(self, clientId, clientSecret, resourceUrl, refreshToken, apiVersion):
+    def __init__(self, client_id, client_secret, resource_url, refresh_token, api_version):
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.resource_url = urljoin(resource_url, 'api/data', api_version)
+        self.resource_url_base = urljoin(resource_url, '')
+        self.refresh_token = refresh_token
+        self.api_version = api_version
+        super().__init__(base_url=resource_url, max_retries=self.MAX_RETRIES)
+        self.refresh_access_token()
+        self.api_endpoints = self.get_api_endpoints()
 
-        self.parClientId = clientId
-        self.parClientSecret = clientSecret
-        self.parResourceUrl = os.path.join(resourceUrl, 'api/data', apiVersion)
-        self.parResourceUrlBase = os.path.join(resourceUrl, '')
-        self.parRefreshToken = refreshToken
-        self.parApiVersion = apiVersion
-
-        super().__init__(base_url=self.parResourceUrl, max_retries=self.MAX_RETRIES)
-        _accessToken = self.refreshToken()
-
-        _defHeader = {
-            'Authorization': f'Bearer {_accessToken}',
-            'Accept': 'application/json'
-        }
-
-        self._auth_header = _defHeader
-        self.getEntityMetadata()
-
-    def refreshToken(self):
-
-        self._auth_header = {}
-
-        headersRefresh = {
+    def refresh_access_token(self):
+        headers = {
             'Content-Type': 'application/x-www-form-urlencoded',
             'Accept': 'application/json'
         }
-
-        bodyRefresh = {
-            'client_id': self.parClientId,
+        data = {
+            'client_id': self.client_id,
             'grant_type': 'refresh_token',
-            'client_secret': self.parClientSecret,
-            'resource': self.parResourceUrlBase,
-            'refresh_token': self.parRefreshToken
+            'client_secret': self.client_secret,
+            'resource': self.resource_url,
+            'refresh_token': self.refresh_token
         }
+        logging.debug("Refreshing access token.")
+        response = self.post_raw(url=self.MSFT_LOGIN_URL, headers=headers, data=data)
+        status_code, response_body = response.status_code, response.json()
 
-        reqRefresh = self.post_raw(url=self.MSFT_LOGIN_URL, headers=headersRefresh, data=bodyRefresh)
-        scRefresh, jsRefresh = reqRefresh.status_code, reqRefresh.json()
+        if status_code == 200:
+            logging.debug("Token refreshed successfully.")
+            access_token = response_body['access_token']
+            header = {
+                'Authorization': f'Bearer {access_token}',
+                'Accept': 'application/json'
+            }
+            self._auth_header = header
+            return access_token
 
-        if scRefresh == 200:
-            logging.debug("Access token refreshed successfully.")
-            return jsRefresh['access_token']
+        logging.error(f"Could not refresh access token. Received {status_code} - {response_body}.")
+        sys.exit(1)
 
-        else:
-            logging.error(f"Could not refresh access token. Received {scRefresh} - {jsRefresh}.")
-            sys.exit(1)
-
-    def __response_hook(self, res, *args, **kwargs):
-
-        if res.status_code == 401:
-            token = self.refreshToken()
+    def __response_hook(self, response, *args, **kwargs):
+        if response.status_code == 401:
+            token = self.refresh_access_token()
             self._auth_header = {"Authorization": f'Bearer {token}',
                                  "Accept": "application/json"}
 
-            res.request.headers['Authorization'] = f'Bearer {token}'
-            s = requests.Session()
-            return self.requests_retry_session(session=s).send(res.request)
+            response.request.headers['Authorization'] = f'Bearer {token}'
+            session = requests.Session()
+            return self.requests_retry_session(session=session).send(response.request)
 
     def requests_retry_session(self, session=None):
-
         session = session or requests.Session()
         retry = Retry(
             total=self.max_retries,
@@ -89,69 +79,64 @@ class DynamicsClient(HttpClientBase):
         session.hooks['response'].append(self.__response_hook)
         return session
 
-    def getEntityMetadata(self):
+    def get_api_endpoints(self):
+        url = urljoin(self.base_url, 'EntityDefinitions')
+        params = {'$select': 'PrimaryIdAttribute,EntitySetName'}
 
-        urlMeta = os.path.join(self.base_url, 'EntityDefinitions')
-        paramsMeta = {
-            '$select': 'PrimaryIdAttribute,EntitySetName'
-        }
-
-        reqMeta = self.get_raw(url=urlMeta, params=paramsMeta)
-        scMeta, jsMeta = reqMeta.status_code, reqMeta.json()
-
-        if scMeta == 200:
-
-            logging.debug("Obtained logical definitions of entities.")
-            self.varApiObjects = {e['EntitySetName'].lower(): e for e in jsMeta['value']
-                                  if e['EntitySetName'] is not None}
-
-        else:
-
-            logging.error("Could not obtain entity metadata for resource.")
-            logging.error(f"Received: {scMeta} - {jsMeta}.")
+        try:
+            response = self.get_raw(url=url, params=params)
+        except requests.exceptions.ConnectionError as e:
+            logging.error("Could not obtain logical object definitions. Please, check the "
+                          f"organization URL or authorization.\n{e}")
+            sys.exit(1)
+        except requests.exceptions.RetryError as e:
+            logging.error("Could not obtain logical object definitions. Please, check the "
+                          f"the supported API version in correct format (v9.0, v9.1, etc.) is specified.\n{e}")
             sys.exit(1)
 
-    def downloadData(self, endpoint, query=None, nextLinkUrl=None, download_formatted_values=False):
+        status_code, response_body = response.status_code, response.json()
 
+        if status_code == 200:
+            logging.debug("Obtained logical definitions of entities.")
+            endpoints_metadata = {
+                entity['EntitySetName'].lower(): entity
+                for entity in response_body['value']
+                if entity['EntitySetName'] is not None
+            }
+            return endpoints_metadata
+
+        logging.error("Could not obtain entity metadata for resource.")
+        logging.error(f"Received: {status_code} - {response_body}.")
+        sys.exit(1)
+
+    def download_data(self, endpoint, query=None, next_link_url=None, download_formatted_values=False):
         prefer_value = f"odata.maxpagesize={self.PAGE_SIZE}"
         if download_formatted_values:
             prefer_value = f'{prefer_value}, odata.include-annotations="OData.Community.Display.V1.FormattedValue"'
 
-        headersQuery = {
-            'Prefer': prefer_value
-        }
+        headers = {'Prefer': prefer_value}
 
-        if nextLinkUrl is not None and nextLinkUrl != '':
-            urlQuery = nextLinkUrl
-
+        if next_link_url:
+            url = next_link_url
         else:
-            urlQuery = os.path.join(self.base_url, endpoint)
+            url = urljoin(self.base_url, endpoint)
+            if query is not None:
+                url += '?' + query
 
-            if query is not None and query != '':
-                urlQuery += '?' + query
+        response = self.get_raw(url=url, headers=headers)
+        status_code, response_body = response.status_code, response.json()
 
-        reqQuery = self.get_raw(url=urlQuery, headers=headersQuery)
-        scQuery, jsQuery = reqQuery.status_code, reqQuery.json()
+        if status_code == 200:
+            results = response_body['value']
+            next_link_url = response_body.get('@odata.nextLink')
+            return results, next_link_url
 
-        if scQuery == 200:
+        error_msg = response_body['error']['message']
+        add_msg = ''
+        if 'Could not find a property named' in error_msg:
+            add_msg = 'When querying foreign key fields, do not forget to ommit "fk" part of the field, e.g. ' + \
+                       '"fk_accountid" -> "_accountid". Please, refer to the documentation for more information.'
 
-            _results = jsQuery['value']
-            _nextLink = jsQuery.get('@odata.nextLink', None)
-
-            return _results, _nextLink
-
-        else:
-
-            _err_msg = jsQuery['error']['message']
-
-            if 'Could not find a property named' in _err_msg:
-                _add_msg = 'When querying foreign key fields, do not forget to ommit "fk" part of the field, e.g. ' + \
-                           '"fk_accountid" -> "_accountid". Please, refer to the documentation for more information.'
-
-            else:
-                _add_msg = ''
-
-            logging.error(''.join([f"Could not query endpoint \"{endpoint}\". ",
-                                   f"Received: {scQuery} - {_err_msg} ",
-                                   _add_msg]))
-            sys.exit(1)
+        logging.error(
+            f"Could not query endpoint \"{endpoint}\". Received: {status_code} - {error_msg}{add_msg}")
+        sys.exit(1)
--- src/dynamics/component.py
+++ src/dynamics/component.py
@@ -6,9 +6,12 @@ from kbc.env_handler import KBCEnvHandler
 from dynamics.client import DynamicsClient
 from dynamics.result import DynamicsWriter
 
+from keboola.component.base import sync_action
+from keboola.component.sync_actions import SelectElement
+
 APP_VERSION = '1.0.2'
 
-KEY_ORGANIZATIONURL = 'organization_url'
+KEY_ORGANIZATION_URL = 'organization_url'
 KEY_ENDPOINT = 'endpoint'
 KEY_API_VERSION = 'api_version'
 KEY_INCREMENTAL = 'incremental'
@@ -16,12 +19,12 @@ KEY_QUERY = 'query'
 KEY_DEBUG = 'debug'
 KEY_DOWNLOAD_FORMATTED_VALUES = "download_formatted_values"
 
-MANDATORY_PARAMS = [KEY_ORGANIZATIONURL, KEY_ENDPOINT, KEY_API_VERSION]
+MANDATORY_PARAMS = [KEY_ORGANIZATION_URL, KEY_ENDPOINT, KEY_API_VERSION]
 
-AUTH_APPKEY = 'appKey'
-AUTH_APPSECRET = '#appSecret'
+AUTH_APP_KEY = 'appKey'
+AUTH_APP_SECRET = '#appSecret'
 AUTH_APPDATA = '#data'
-AUTH_APPDATA_REFRESHTOKEN = 'refresh_token'
+AUTH_APPDATA_REFRESH_TOKEN = 'refresh_token'
 
 sys.tracebacklimit = 0
 
@@ -29,38 +32,34 @@ sys.tracebacklimit = 0
 class DynamicsComponent(KBCEnvHandler):
 
     def __init__(self):
-
         super().__init__(mandatory_params=MANDATORY_PARAMS, log_level='INFO')
-        logging.info("Running component version %s..." % APP_VERSION)
+        logging.info(f"Running component version {APP_VERSION}...")
         self.validate_config(MANDATORY_PARAMS)
 
-        if self.cfg_params.get(KEY_DEBUG, False) is True:
+        if self.cfg_params.get(KEY_DEBUG, False):
             logger = logging.getLogger()
             logger.setLevel(level='DEBUG')
-
             sys.tracebacklimit = 3
 
         auth = self.get_authorization()
-        self.parClientId = auth[AUTH_APPKEY]
-        self.parClientSecret = auth[AUTH_APPSECRET]
+        self.client_id = auth[AUTH_APP_KEY]
+        self.client_secret = auth[AUTH_APP_SECRET]
 
-        authData = json.loads(auth[AUTH_APPDATA])
-        self.parRefreshToken = authData[AUTH_APPDATA_REFRESHTOKEN]
+        auth_data = json.loads(auth[AUTH_APPDATA])
+        self.refresh_token = auth_data[AUTH_APPDATA_REFRESH_TOKEN]
 
-        self.parEndpoint = self.cfg_params[KEY_ENDPOINT].lower()
-        self.parTable = self.parEndpoint + '.csv'
-        self.parApiVersion = self.cfg_params[KEY_API_VERSION]
-        self.parResourceUrl = self.cfg_params[KEY_ORGANIZATIONURL]
-        self.parQuery = '&'.join([q for q in self.cfg_params.get(KEY_QUERY, '').split('\n') if q != ''])
-        self.parIncremental = bool(self.cfg_params.get(KEY_INCREMENTAL, True))
+        self.endpoint = self.cfg_params[KEY_ENDPOINT].lower()
+        self.table = self.endpoint + '.csv'
+        self.api_version = self.cfg_params[KEY_API_VERSION]
+        self.resource_url = self.cfg_params[KEY_ORGANIZATION_URL]
+        self.query = '&'.join([q for q in self.cfg_params.get(KEY_QUERY, '').split('\n') if q != ''])
+        self.incremental = bool(self.cfg_params.get(KEY_INCREMENTAL, True))
         self.download_formatted_values = bool(self.cfg_params.get(KEY_DOWNLOAD_FORMATTED_VALUES, False))
-
-        self.client = DynamicsClient(self.parClientId, self.parClientSecret,
-                                     self.parResourceUrl, self.parRefreshToken,
-                                     self.parApiVersion)
+        self.client = DynamicsClient(
+            self.client_id, self.client_secret, self.resource_url, self.refresh_token, self.api_version)
+        self.writer = None
 
     def get_authorization(self):
-
         try:
             return self.configuration.config_data["authorization"]["oauth_api"]["credentials"]
         except KeyError:
@@ -68,53 +67,44 @@ class DynamicsComponent(KBCEnvHandler):
             exit(1)
 
     def run(self):
-
-        if self.parEndpoint not in self.client.varApiObjects:
-
-            logging.error(' '.join([f"Endpoint \"{self.parEndpoint}\" is not supported by your Dynamics instance.",
-                                    "Please, refer to documentation at",
-                                    "https://docs.microsoft.com/en-us/dynamics365/" +
-                                    "customer-engagement/web-api/entitytypes for",
-                                    "the list of default available resources; or visit",
-                                    f"{os.path.join(self.client.parResourceUrl, 'EntityDefinitions')}",
-                                    "for a complete list of all objects supported by your Dynamics instance."]))
+        if self.endpoint not in self.client.api_endpoints:
+            logging.error(
+                f"Endpoint \"{self.endpoint}\" is not supported by your Dynamics instance. "
+                f"Please, refer to documentation at "
+                f"https://docs.microsoft.com/en-us/dynamics365/customer-engagement/web-api/entitytypes for"
+                "the list of default available resources; or visit"
+                f"{os.path.join(self.client.resource_url, 'EntityDefinitions')}"
+                "for a complete list of all objects supported by your Dynamics instance.")
             sys.exit(1)
 
-        else:
+        logging.info(f"Downloading data for endpoint \"{self.endpoint}\".")
+        has_more = True
+        next_link = None
+        request_count = 0
+        primary_key = self.client.api_endpoints[self.endpoint]['PrimaryIdAttribute']
 
-            logging.info(f"Downloading data for endpoint \"{self.parEndpoint}\".")
+        while has_more:
+            request_count += 1
+            results, next_link = self.client.download_data(
+                self.endpoint, query=self.query, next_link_url=next_link,
+                download_formatted_values=self.download_formatted_values)  # noqa
 
-            _has_more = True
-            _has_wrtr = False
-            _next_link = None
-            _req_count = 0
-            _pk = self.client.varApiObjects[self.parEndpoint]['PrimaryIdAttribute']
+            if self.writer is None:
+                if not results:
+                    logging.info(f"No data returned for endpoint \"{self.endpoint}\".")
+                    sys.exit(0)
 
-            while _has_more is True:
+                self.writer = DynamicsWriter(
+                    self.tables_out_path, self.endpoint, results, [primary_key], self.incremental)
 
-                _req_count += 1
-                _results, _next_link = self.client.downloadData(self.parEndpoint,
-                                                                query=self.parQuery,
-                                                                nextLinkUrl=_next_link,
-                                                                download_formatted_values=self.download_formatted_values)  # noqa
+            self.writer.writerows(results)
+            has_more = True if next_link else False
 
-                if _has_wrtr is False:
+            if not request_count % 20:
+                logging.info(f"Made {request_count} requests to the API so far.")
 
-                    if len(_results) == 0:
-                        logging.info(f"No data returned for endpoint \"{self.parEndpoint}\".")
-                        _has_more = False
-                        sys.exit(0)
+        logging.info(f"Made {request_count} requests to the API in total for endpoint \"{self.endpoint}\".")
 
-                    else:
-                        self.writer = DynamicsWriter(self.tables_out_path, self.parEndpoint, _results,
-                                                     [_pk], self.parIncremental)
-
-                        _has_wrtr = True
-
-                self.writer.writerows(_results)
-                _has_more = True if _next_link else False
-
-                if _req_count % 20 == 0:
-                    logging.info(f"Made {_req_count} requests to the API so far.")
-
-        logging.info(f"Made {_req_count} requests to the API in total for endpoint \"{self.parEndpoint}\".")
+    @sync_action('get_endpoints')
+    def get_endpoints(self):
+        return [SelectElement(endpoint) for endpoint in self.client.api_endpoints]
